308d3d14f7d398d8aef52a5c6a24849a1b78458a
HoldenCaulfieldICL <adalyac@gmail.com>
2014-10-22 19:57:21 +0100

    bring_more script working. now going to replace parameter values for the proper thing

--- scripts/data_preparation/bring_more_redbox.py
+++ scripts/data_preparation/bring_more_redbox.py
@@ -9,13 +9,14 @@ if __name__ == '__main__':
   task = 'scrape'
   classification = ' 0'
   using_pickle = True
+  add_num = 20000 # how many imgs to add
   pickle_fname = 'redbox_vacant_'+task+'_negatives.pickle'
-  redbox = '/data2/ad6813/pipe-data/Redbox/raw_data/dump/'
-  fn_train = '/data2/ad6813/caffe/data/scrape/train.txt'
+  redbox = '/data2/ad6813/pipe-data/Redbox/raw_data/temp/' #DUMP
+  fn_train = '/data2/ad6813/caffe/data/scrape/fake.txt'    #TRAIN
 
   if os.path.isfile(oj(os.getcwd(),'redbox_vacant_'+task+'_negatives.pickle')) and using_pickle:
     print "Found pickle dump of vacant, non-perfect Redbox images without %s flag. Using it."%(task)
-    notperf = pickle.load(pickle_fname,'rb')
+    notperf = pickle.load(open(pickle_fname,'r'))
 
   else:
     notperf, total = [], []
@@ -26,25 +27,26 @@ if __name__ == '__main__':
     print 'Gathering vacant, non-perfect Redbox images without %s flag...'%(task)
     with open(fn_train,'r') as f_already:
       c_already = f_already.readlines()
+      c_already = [line.split(' ')[0] for line in c_already]
       for i in range(len(total)):
         content = open(oj(redbox,total[i]),'r').readlines()
-        if len(content) > 0: 
-          if not any([total[i] in l_already for l_already in c_already]):
-            notperf.append(redbox+total[i][:-4]+'.jpg'+classification)
+        if len(content) > 0:
+          if total[i] not in c_already:
+            notperf.append(redbox+total[i][:-4]+'.jpg'+classification+'\n')
 
     random.shuffle(notperf)
-    print "Gathering completed. Pickle dumping it."
-    pickle.dump(notperf, open(pickle_fname,'wb'))
+    print "Gathering completed."
 
-  newcomers, notperf_left = notperf[:20000], notperf[20000:]
-  pickle.dump(notperf_left, open(pickle_fname,'wb'))
-  print "% updated"%(pickle_fname)
+  newcomers, notperf_left = notperf[:add_num], notperf[add_num:]
+  pickle.dump(notperf_left, open(pickle_fname,'w'))
+  print "%s updated"%(pickle_fname)
 
   with open(fn_train,'r') as f_train:
     c_train = f_train.readlines()
   c_train += newcomers
   random.shuffle(c_train)
   f_train = open(fn_train,'w')
+  # print "writing:", c_train
   f_train.writelines(c_train)
 
 
