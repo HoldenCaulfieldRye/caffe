aa5ec4dbe2384be5ba99941d418bb513434bdbdb
HoldenCaulfieldICL <adalyac@gmail.com>
2014-10-23 02:26:44 +0100

    undersample script pretty good now

---
+++ scripts/data_preparation/undersample.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+
+# delete_some_files('/data2/ad6813/caffe/data/scrape/train_2.txt','0',5400)
+def delete_some_files(fname, class_num, del_num):
+  count, new = 0, []
+  content = open(fname, 'r').readlines()
+  for line in content:
+    if line.endswith(class_num+'\n') and count < del_num: count += 1
+    else: new.append(line)
+  open(fname,'w').writelines(new)
+
+def print_help():
+  print "Usage: ./undersample.py path/to/file class_num del_num"
+  print "Example: ./undersample.py /data2/ad6813/caffe/data/scrape/train_2.txt 0 5400"
+
+if __name__ == '__main__':
+  if len(sys.argv) < 4:
+    print_help()
+    exit
+  else:
+    delete_some_files(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+
